e271a946bb96f4fc1d6586d1715dd3a540502637
Fábio Ferreira <ffcfpten@gmail.com>
2019-07-07 05:17:39 +0100

    feat: scale svg animations for increasing and decreasing scale

--- lottie-scale.js
+++ lottie-scale.js
@@ -5,18 +5,23 @@
  * @param {HTMLElement} container
  */
 function lottieScale(multiplier, container) {
-  if (multiplier < 0) return Error('Scale should always be a positive floating number')
+  if (multiplier < 1) return Error('Scale should always be a positive floating number greater than 1')
   
   let svgElement = container.querySelector('svg')
   
-  if (multiplier > 1) {
-    // reduce size by multipler-th times
-    // For example, an SVG with width 1920px would now become 960px wide.
-    // The same happens with height
-    svgElement.viewBox.baseVal.width /= multiplier
-    svgElement.viewBox.baseVal.height /= multiplier
+  let newFrameWidth = svgElement.viewBox.baseVal.width / multiplier
+  let newFrameHeight = svgElement.viewBox.baseVal.height / multiplier
 
-    svgElement.viewBox.baseVal.x = svgElement.viewBox.baseVal.width / multiplier
-    svgElement.viewBox.baseVal.y = svgElement.viewBox.baseVal.height / multiplier
-  }
+  // For example:
+  // An SVG animation with width 1920px would now become 960px wide with scale 2.
+  // The same happens with height
+  svgElement.width.baseVal.value = svgElement.viewBox.baseVal.width = newFrameWidth
+  svgElement.height.baseVal.value = svgElement.viewBox.baseVal.height = newFrameHeight
+
+  // Only change the starting X and Y coords if the frame size is decreasing
+  let x = newFrameWidth / 2,
+      y = newFrameHeight / 2
+
+  svgElement.viewBox.baseVal.x = x - ((multiplier % 1) * x)
+  svgElement.viewBox.baseVal.y = y - ((multiplier % 1) * y)
 }
