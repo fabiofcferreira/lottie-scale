96e514903849f8ea5175c53b655634c0e551f684
Fábio Ferreira <ffcfpten@gmail.com>
2019-07-07 07:51:45 +0100

    chores: add linting

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@
  * @param {Number} multiplier Scale multiplier of the size
  * @param {HTMLElement} container
  */
-function lottieScale(multiplier, container) {
+function lottieScale (multiplier, container) {
   if (multiplier < 1) return Error('Scale should always be a positive floating number greater than 1')
-  
+
   let svgElement = container.querySelector('svg')
-  
+
   let newFrameWidth = svgElement.viewBox.baseVal.width / multiplier
   let newFrameHeight = svgElement.viewBox.baseVal.height / multiplier
 
@@ -19,9 +19,11 @@ function lottieScale(multiplier, container) {
   svgElement.height.baseVal.value = svgElement.viewBox.baseVal.height = newFrameHeight
 
   // Only change the starting X and Y coords if the frame size is decreasing
-  let x = newFrameWidth / 2,
-      y = newFrameHeight / 2
+  let x = newFrameWidth / 2
+  let y = newFrameHeight / 2
 
   svgElement.viewBox.baseVal.x = x - ((multiplier % 1) * x)
   svgElement.viewBox.baseVal.y = y - ((multiplier % 1) * y)
 }
+
+module.exports = lottieScale
